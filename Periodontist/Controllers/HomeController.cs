f460b7803053322a9a036e84e8f4c19aeb13e0c0
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2017-04-05 15:10:31 +0300

    Общие изменения

--- Periodontist/Controllers/HomeController.cs
+++ Periodontist/Controllers/HomeController.cs
@@ -13,12 +13,7 @@ namespace Periodontist.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
+       
 
         public IActionResult Contact()
         {
