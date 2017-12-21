8f9406104b257dea32300abfa7d2910f63d62135
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2017-12-21 15:00:31 +0300

    Добавил базу и модель вопроса пользователя, а также контекст данных

---
+++ periodontist/Models/PeriodontistContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace periodontist.Models
+{
+  public class PeriodontistContext : DbContext
+  {
+    public DbSet<UserQuestion> UserQuestions { get; set; }
+  }
+}
