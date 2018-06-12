9204111f0a625cb71e56988c8ed53bb2e2b9db4a
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2018-06-12 17:10:45 +0300

    установил npm, удалил vue-upload-component

--- periodontist/Controllers/HomeController.cs
+++ periodontist/Controllers/HomeController.cs
@@ -10,87 +10,94 @@ using System.Web.Mvc;
 
 namespace periodontist.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    PeriodontistContext db = new PeriodontistContext();
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View(db.UserQuestions);
-    }
 
-
-    [HttpPost]
-    public JsonResult SendQuestion(UserQuestion question)
-    {
-      bool result = false;
-      try
-      {
-        db.UserQuestions.Add(question);
-        db.SaveChanges();
-        result = true;
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
-
-      return Json(new
-      {
-        res = result
-      });
-    }
-
-    [HttpPost]
-    public async Task<JsonResult> Upload()
-    {
-      var res = "";
-      var folderGuid = Guid.NewGuid();
-      try
-      {
-        foreach (string file in Request.Files)
+        PeriodontistContext db = new PeriodontistContext();
+        public ActionResult Index()
         {
-          var fileContent = Request.Files[file];
-          if (fileContent != null && fileContent.ContentLength > 0)
-          {
-            var fileName = fileContent.FileName;
-            if (!Directory.Exists(Server.MapPath("~/Uploads/UserFiles/"+folderGuid)))
-            {
-              Directory.CreateDirectory(Server.MapPath("~/Uploads/UserFiles/"+ folderGuid));
-            }
-            var path = Path.Combine(Server.MapPath("~/Uploads/UserFiles/" + folderGuid), fileName);
-            fileContent.SaveAs(path);
-          }
+            return View(db.UserQuestions);
         }
-      }
-      catch (Exception)
-      {
-        Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        return Json("Upload failed");
-      }
 
-      return Json(folderGuid);
+
+        [HttpPost]
+        public JsonResult SendQuestion(UserQuestion question)
+        {
+            bool result = false;
+            try
+            {
+                db.UserQuestions.Add(question);
+                db.SaveChanges();
+                result = true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Json(new
+            {
+                res = result
+            });
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Upload()
+        {
+            var res = "";
+            var folderGuid = Guid.NewGuid();
+            var count = Request.Files.Count;
+            try
+            {
+
+                foreach (string file in Request.Files)
+                {
+                    var fileContent = Request.Files[file];
+                    if (fileContent != null && fileContent.ContentLength > 0)
+                    {
+                        var fileName = fileContent.FileName;
+                        if (!Directory.Exists(Server.MapPath("~/Uploads/UserFiles/" + folderGuid)))
+                        {
+                            Directory.CreateDirectory(Server.MapPath("~/Uploads/UserFiles/" + folderGuid));
+                        }
+                        var path = Path.Combine(Server.MapPath("~/Uploads/UserFiles/" + folderGuid), fileName);
+                        fileContent.SaveAs(path);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("Upload failed");
+            }
+
+            return Json(new
+            {
+                folderName = folderGuid
+                ,
+                c = count
+            });
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
-
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
-
-      return View();
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-      db.Dispose();
-      base.Dispose(disposing);
-    }
-  }
 }
