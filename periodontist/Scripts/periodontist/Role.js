238d1ee161aced383ad7174e8407c01af3b460bc
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2018-09-26 15:28:34 +0300

    Реализовано добавление роли

--- periodontist/Scripts/periodontist/Role.js
+++ periodontist/Scripts/periodontist/Role.js
@@ -15,7 +15,6 @@ role = {
             , data: {
                 title: role.config.title
                 , rolesListData: []
-                //,queryResponse
             }
             , created: function () {
                 this.getList();
@@ -31,40 +30,27 @@ role = {
                         });
                 }
                 , newRole: function () {
-                    $("#addNewRole").modal()
-
+                    $("#addNewRole").modal();
                     
                 }
                 , addNewRole: function () {
-                    console.info("add new role");
-                    //axios.post('/Role/Create', new { Name: "name", Description: "description" })
-                    //    .then(function (response) {
-                    //        rolesList.queryResponse = response.data.result;
-                    //    }).catch(function (error) {
-                    //        console.log(error);
-                    //    });
+
+                    var roleName = $(".addNewRole_name").val();
+                    var roleDescription = $(".addNewRole_Description").val();
+
+                    axios.post('/Role/Create', { Name: roleName, Description: roleDescription })
+                        .then(function (response) {
+                            $("#addNewRole").modal('hide');
+                            rolesList.getList();
+                        }).catch(function (error) {
+                            console.log(error);
+                        });
                 }
             }
 
         });
     }
-
-    , addNewRole: function () {
-        console.info("add role function");
-        var addRole = $.ajax({
-            url: "/Role/Create"
-            , type: "POST"
-            , dataType: "json"
-            , success: function (data) {
-                if (data) {
-                    console.info(JSON.parse(JSON.stringify(data.result)));
-                }
-                return JSON.parse(JSON.stringify(data.result));
-            }
-        });
-
-
-    }
+   
 };
 $(document).ready(
     function () {
