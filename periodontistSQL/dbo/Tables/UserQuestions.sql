b5ae5e4fc925ed36ff0a7db26cf1f8e446e0d6d5
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2019-04-29 17:30:44 +0300

    добавлен проект базы данных

---
+++ periodontistSQL/dbo/Tables/UserQuestions.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserQuestions] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (MAX) NULL,
+    [Email]        NVARCHAR (MAX) NULL,
+    [Theme]        NVARCHAR (MAX) NULL,
+    [QuestionText] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.UserQuestions] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
