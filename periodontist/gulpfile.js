24cbe32040a4bf5d88472fa4be1a8f8cefba3a12
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2019-07-09 17:53:15 +0300

    подключил стили через галп

--- periodontist/gulpfile.js
+++ periodontist/gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-    gulp.task('mytask', function() {
-        console.log('Привет, я таск!');
-    });
+gulp.task('sass', function(){ // Создаем таск "sass"
+    return gulp.src('content/Site.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('content/')); // Выгружаем результата в папку app/css
+});
